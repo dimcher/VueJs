1b3ae9cf4ec7359c2ddb8b91508d550853dba820
Dima Chernyshev <dimcher@gmail.com>
2021-08-11 17:22:58 -0400

    delete added

---
+++ Test/src/components/GoogleId.vue
@@ -0,0 +1,35 @@
+<template>
+    <div>
+        <div class="sign">Please enter Google ID</div>
+        <p><input type="text" name="googleId" /></p>
+        <button :name="googleId" @click="getGoogleId">SUBMIT</button>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'GoogleId',
+    props: {
+    },
+    data: () => {
+        return {
+            googleId: ''
+        }
+    },
+    methods: {
+        getGoogleId: function () {
+            const id = document.querySelector("input[name=googleId]").value;
+            this.$emit('getGoogleId', id);
+        }
+
+    }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+    .sign { color: red; }
+    input { width: 300px; }
+    button { font-weight: bold; }
+    
+</style>
---
+++ Test/src/components/OptionEdit.vue
@@ -0,0 +1,46 @@
+<template>
+    <td><button @click="refreshList">EDIT</button></td>
+</template>
+
+<script>
+import axios from 'axios';
+export default {
+    name: 'OptionEdit',
+    props: {
+        optionId: {
+            type: Number
+        },
+        googleId: {
+            style: String
+        }
+    },
+    data: () => {
+        return {
+            errors: []
+        }
+    },
+    methods: {
+        deleteOption() {
+            console.log("Edit");
+            let that = this;
+            axios
+                .patch('https://services.metricsamsi.com/v1.0/dealers/Options/'+ that.optionId +'?apiKey='+ that.googleId)
+                .then(function () {
+                    this.refreshList();
+                })
+                .catch(e => {
+                    that.errors.push(e);
+                });
+        },
+        refreshList: function () {
+            this.$emit('refreshMe');
+        }
+
+    }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+    
+</style>
